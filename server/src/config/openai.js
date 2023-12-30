fd16e0fef4d826ff99b050bb5534bba18be46569
Tadeu Raphael <tadeu.raphael99@protonmail.com>
2023-12-30 17:24:37 -0300

    Patch: Adicionado o front e customizado layout

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -1,23 +1,24 @@
-import OpenAI from "openai";
+const { Configuration, OpenAIApi } = require('openai')
+require("dotenv").config()
 
 module.exports = class openai{
     static configuration(){
-        const openai = new OpenAI({
-            apiKey: process.env.OPENAI_API_KEY,
-        });
+		const configuration = new Configuration({
+			apiKey: process.env.OPEN_AI_KEY,
+		})
 
-        return new OpenAIApi(openai)
-    }
+		return new OpenAIApi(configuration)
+	}
 
-    static textCompletion({prompt}){
-        return {
-            model: "gpt-3.5-turbo-instruct",
-            prompt: `${prompt}`,
-            temperature: 0,
+	static textCompletion ({prompt}) {
+        return 	{
+            model:"text-davinci-003",
+            prompt:`${prompt}`,
+            temperature:0,
             max_tokens: 3500,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
+            top_p:1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
         }
     }
 }
--- server/src/controllers/prompt-controller.js
+++ server/src/controllers/prompt-controller.js
@@ -18,7 +18,7 @@ module.exports = {
         } catch (error) {
             return res.status(400).json({
                 sucess: false,
-                error: error.response ? error.response : "There was an issue..."
+                error: error.response ? error.response.data : "There was an issue..."
             })
         }
     }
